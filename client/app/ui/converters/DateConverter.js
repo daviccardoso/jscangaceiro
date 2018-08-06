43ef4df73ed54e21200aed4a0c32a89d525d27e4
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2018-08-06 15:08:18 -0300

    Remoção de linha em branco da classe DateConverter

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -3,8 +3,7 @@ class DateConverter {
         throw new Error('Esta classe não pode ser instanciada.');
     }
 
-    static paraTexto(data) {
-        
+    static paraTexto(data) {        
         return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
     
