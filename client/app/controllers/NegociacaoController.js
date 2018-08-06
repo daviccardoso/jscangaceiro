4d777086101b94c54f0acd6d63cecc8cb42f5c21
Davi Cardoso <daviccardoso@outlook.com>
2018-08-06 13:55:14 -0300

    Atualização da conversão de data na classe NegociacaoController

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,7 +9,7 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value.split('-').map((item, indice) => indice === 1 ? item - 1 : item));
+        let data = new Date(...this._inputData.value.split('-').map((item, indice) => item - indice % 2));
         let negociacao = new Negociacao(
             data,
             parseInt(this._inputQuantidade.value),
