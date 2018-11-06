bf8a8cedd2193b0bc8afb1a17af545c533a6412e
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2018-11-06 16:30:37 -0200

    Criação de requisição POST através da API Fetch

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -6,3 +6,19 @@ const $ = document.querySelector.bind(document);
 $('.form').addEventListener('submit', controller.adiciona.bind(controller));
 $('#botao-apaga').addEventListener('click', controller.apaga.bind(controller));
 $('#botao-importa').addEventListener('click', controller.importaNegociacoes.bind(controller));
+
+const negociacao = { data: '2018-11-06', quantidade: 1, valor: 200 };
+const headers = new Headers();
+const body = JSON.stringify(negociacao);
+const method = 'POST';
+
+headers.set('Content-Type', 'application/json');
+
+const config = {
+  method,
+  headers,
+  body
+};
+
+fetch('/negociacoes', config)
+  .then(() => console.log('Dados enviados com sucesso.'));
