f72a98e99df3aba76ced64048899f7940936ef7c
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2018-11-06 11:48:58 -0200

    Adoção da API fetch para importação de negociações

--- client/app-src/util/HttpService.js
+++ client/app-src/util/HttpService.js
@@ -1,21 +1,12 @@
 export class HttpService {
   get(url) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
+    return fetch(url)
+      .then(res => this._handleErrors(res))
+      .then(res => res.json());
+  }
 
-      xhr.open('GET', url);
-      xhr.addEventListener('readystatechange', () => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            console.log(xhr.responseText);
-            reject(xhr.responseText);
-          }
-        }
-      });
-
-      xhr.send();
-    });
+  _handleErrors(res) {
+    if (!res.ok) throw new Error(res.statusText);
+    return res;
   }
 }
