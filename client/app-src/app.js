3daffc10b93df5f565b0398d58bdf08686b778b7
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2018-11-06 21:27:27 -0200

    Melhoria na requisição POST de app.js

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -7,7 +7,12 @@ $('.form').addEventListener('submit', controller.adiciona.bind(controller));
 $('#botao-apaga').addEventListener('click', controller.apaga.bind(controller));
 $('#botao-importa').addEventListener('click', controller.importaNegociacoes.bind(controller));
 
-const negociacao = { data: '2018-11-06', quantidade: 1, valor: 200 };
+const dataAtual = new Date();
+const negociacao = {
+  data: `${dataAtual.getFullYear()}/${dataAtual.getMonth() + 1}/${dataAtual.getDate()}`,
+  quantidade: 3,
+  valor: 125
+};
 const headers = new Headers();
 const body = JSON.stringify(negociacao);
 const method = 'POST';
