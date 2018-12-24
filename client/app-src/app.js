0fffd3f792b94f2bac4d373b54ce01ee81730cca
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2018-12-24 20:55:33 -0200

    Implementação de importação de arquivos CSS como módulos

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import { NegociacaoController } from './controllers/NegociacaoController.js';
 
 const controller = new NegociacaoController();
