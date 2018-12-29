6a2824980efeb6db05390cd1fe7bbe8e35923259
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2018-12-29 12:45:32 -0200

    Separação de código da aplicação e bibliotecas em diferentes bundles

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,6 +11,10 @@ plugins.push(new webpack.ProvidePlugin({
   $: 'jquery/dist/jquery.js',
   jQuery: 'jquery/dist/jquery.js'
 }));
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+  name: 'vendor',
+  filename: 'vendor.bundle.js'
+}));
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
@@ -27,7 +31,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = {
-  entry: './app-src/app.js',
+  entry: {
+    app: './app-src/app.js',
+    vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
